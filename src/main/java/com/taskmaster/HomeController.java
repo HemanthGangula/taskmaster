c6649477196cfcbf0e4f8349398425d97e596bc4
Hemanth <81347482+HemanthGangula@users.noreply.github.com>
2025-01-19 03:47:35 +0000

    test full pipeline
    Update main.yml
    
    Update main.yml
    
    test pipeline
    
    Update HomeController.java
    
    Update HomeController.java

--- src/main/java/com/taskmaster/HomeController.java
+++ src/main/java/com/taskmaster/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
                "<h1 style='text-align:center;'>Welcome to Taskmaster!</h1>" +
                "<p style='text-align:center;'> Spring Boot Demonstration  </p>" +
                "<p style='text-align:center;'> By Hemanth Gangula  </p>" +
-               "<p style='text-align:center;'> <a href='http://localhost:8080/tasks'>View Tasks</a> </p>" +
+               //"<p style='text-align:center;'> <a href='http://localhost:8080/tasks'>View Tasks</a> </p>" +
                "</body>" +
                "</html>";
     }
